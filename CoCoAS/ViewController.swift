bcbf371961ae08c88d5122b027c5a47a191d0d55
orihara ayami <karu@sfc.wide.ad.jp>
2016-12-31 15:46:06 +0900

    Swift化。appDelegateまで整備完了

---
+++ CoCoAS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CoCoAS
+//
+//  Created by orihara ayami on 2016/12/31.
+//  Copyright © 2016年 orihara ayami. All rights reserved.
+//
+
+import UIKit 
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoCoAS/objc-Header.h
@@ -0,0 +1,9 @@
+//
+//  Header.h
+//  CoCoAS
+//
+//  Created by orihara ayami on 2016/12/31.
+//  Copyright © 2016年 orihara ayami. All rights reserved.
+//
+
+#import "MicrosoftBandKit_iOS/MicrosoftBandKit_iOS.h"
